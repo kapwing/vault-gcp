344a1eda39c0596a0d7312eefe566e3c68fcf6d4
Kevin Musselman <kmussel@gmail.com>
2025-10-13 15:44:18 -0400

    upgrade build version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vault-gcp",
-    version="0.0.2",
+    version="0.0.3",
     author="Kevin Musselman",
     author_email="kevin@kapwing.com",
     description="Authenticate to vault and retrieve env vars",
